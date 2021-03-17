d9ad98bb9d68b98f4e78e4223cbf84c3a155c7ae
Julia Demel <julia.demel@gmail.com>
2021-03-17 14:47:22 +0100

    add advanced

---
+++ Basic3/basic3.ts
@@ -0,0 +1,22 @@
+// Basic ex 3
+
+// Create an array of names, this array should only accept strings.
+    // 1. Print only the index of the elements.
+    // 2. Print only the values. 
+    // 3. Print both index and values.
+
+    // another type of declaration:
+    // var myArray : string[] = [...]
+var myArray : Array<string> = ["Andrea", "Julia", "another Name"];
+    
+for (let index in myArray) {
+    document.getElementById("output").innerHTML += myArray[index] + "<br>"; // for-in prints actual values
+    // returns the actual names like "Andrea" etc.
+    console.log(index);  // for-in prints actual indexes
+    // returns the index - 0, 1, 2
+}
+
+
+
+
+// array[Number(index)] 
---
+++ advanced/advanced.ts
@@ -0,0 +1,115 @@
+// Day 1 | Classwork | Advanced
+// @ts-check
+// Create a Restaurant Web Page using the below-provided Typescript concepts. Try to implement all of them in an appropriate place in the App:
+
+//   #  Define for each variable/array the data type
+//   ?  List the array items and make sure that it has a strict data type definition
+//   #  Use some loop for displaying the data into the Page (forEach, For in, or For-of)
+//     Define some appropriate value as const
+
+// It is up to you how the front-end part will look like. It will be nice if you provide a well structured and designed Bootstrap page.
+
+// Your task is to incorporate all these features using Typescript in a proper and meaningful place in your App. More dynamic data is always welcome. Feel free to add additional functionalities.
+
+var menuArray: Array<{
+  mealTitle: string;
+  mealImg: string;
+  mealPrice: number;
+  mealDescription: string;
+  mealVeggy: boolean;
+}> = [
+  {
+    mealTitle: "Gambas Al Ajillo",
+    mealImg: "https://www.hola.com/imagenes/cocina/recetas/20191128154911/receta-gambas-al-ajillo/0-750-844/gambas-ajillo-m.jpg",
+    mealPrice: 7.90,
+    mealDescription: "Gambas",
+    mealVeggy: false,
+  },
+  {
+    mealTitle: "Tortilla Espanola",
+    mealImg: "https://www.carolinescooking.com/wp-content/uploads/2015/04/Spanish-tortilla-espanola-photo.jpg",
+    mealPrice: 5.30,
+    mealDescription: "made from potatoes",
+    mealVeggy: true,
+  },
+  {
+    mealTitle: "Patatas Bravas",
+    mealImg: "https://www.pequerecetas.com/wp-content/uploads/2015/07/Patatas-bravas-Madrid-550x550.jpg",
+    mealPrice: 6.30,
+    mealDescription: "made from potatoes",
+    mealVeggy: true,
+  },
+  {
+    mealTitle: "Calamares Fritos",
+    mealImg: "https://unareceta.com/wp-content/uploads/2017/04/calamares-a-la-romana-tiernos.jpg",
+    mealPrice: 12.70,
+    mealDescription: "fresh from Malaga",
+    mealVeggy: false,
+  },{
+    mealTitle: "Gambas Al Ajillo",
+    mealImg: "https://www.hola.com/imagenes/cocina/recetas/20191128154911/receta-gambas-al-ajillo/0-750-844/gambas-ajillo-m.jpg",
+    mealPrice: 7.90,
+    mealDescription: "Gambas",
+    mealVeggy: false,
+  },
+  {
+    mealTitle: "Tortilla Espanola",
+    mealImg: "https://www.carolinescooking.com/wp-content/uploads/2015/04/Spanish-tortilla-espanola-photo.jpg",
+    mealPrice: 5.30,
+    mealDescription: "made from potatoes",
+    mealVeggy: true,
+  },
+  {
+    mealTitle: "Patatas Bravas",
+    mealImg: "https://www.pequerecetas.com/wp-content/uploads/2015/07/Patatas-bravas-Madrid-550x550.jpg",
+    mealPrice: 6.30,
+    mealDescription: "made from potatoes",
+    mealVeggy: true,
+  },
+  {
+    mealTitle: "Calamares Fritos",
+    mealImg: "https://unareceta.com/wp-content/uploads/2017/04/calamares-a-la-romana-tiernos.jpg",
+    mealPrice: 12.70,
+    mealDescription: "fresh from Malaga",
+    mealVeggy: false,
+  }
+];
+
+
+// print our meals into #output
+
+// make a loop
+for (let i = 0; i < menuArray.length; i++){
+    let output : any = document.getElementById("output");
+    // var divContainer = document.createElement("div");
+    // divContainer.setAttribute("class","container");
+    // output.appendChild(divContainer);
+
+    // var divTitle = document.createElement("div");
+    // divTitle.setAttribute("class","mealTitle");
+    // let innerDivTitle = document.createTextNode(menuArray[i].mealTitle);
+    // divTitle.appendChild(innerDivTitle);
+
+    
+    output.innerHTML += `
+        <div class="mealContainer ">
+            <div class="mealTitle">${menuArray[i].mealTitle}</div>
+            <img class="img-thumbnail" src="${menuArray[i].mealImg}">
+            <div>${menuArray[i].mealDescription}</div>
+            <div>€ ${menuArray[i].mealPrice}</div>
+        </div>
+    `
+}
+
+
+// declare a const variable
+
+const contactInformation : string = "info@restaurant.es";
+document.getElementById("footer").innerHTML = contactInformation;
+
+// function printContact(){
+//     const contactInformation : string = "info@restaurant.es";
+//     document.getElementById("footer").innerHTML = contactInformation;
+// }
+// printContact();
+
